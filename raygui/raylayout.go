508941fc9af10806c3009dbcae96526d7b6b50ee
Igor Karatayev <igor.cwer@gmail.com>
2024-03-06 22:28:31 +0300

    Renamed Add to Layout functions. Added Fill functions.

--- raygui/raylayout.go
+++ raygui/raylayout.go
@@ -5,9 +5,11 @@ import (
 )
 
 var (
-	AnchorTopLeft  = rl.NewVector2(0, 0)
-	AnchorTopRight = rl.NewVector2(1, 0)
-	AnchorCenter   = rl.NewVector2(0.5, 0.5)
+	AnchorTopLeft     = rl.NewVector2(0, 0)
+	AnchorTopRight    = rl.NewVector2(1, 0)
+	AnchorCenter      = rl.NewVector2(0.5, 0.5)
+	AnchorBottomLeft  = rl.NewVector2(0, 1)
+	AnchorBottomRight = rl.NewVector2(1, 1)
 )
 
 func Pivot(anchor rl.Vector2, r rl.Rectangle) rl.Rectangle {
@@ -24,10 +26,36 @@ func CanvasLayout(bounds rl.Rectangle) canvasLayout {
 	}
 }
 
-func (cl *canvasLayout) Add(anchor rl.Vector2, pivot rl.Vector2, rect rl.Rectangle) rl.Rectangle {
+func (cl *canvasLayout) Layout(anchor rl.Vector2, pivot rl.Vector2, rect rl.Rectangle) rl.Rectangle {
 	anchorp := rl.NewVector2(anchor.X*cl.bounds.Width, anchor.Y*cl.bounds.Height)
 	pivotp := rl.NewVector2(pivot.X*rect.Width, pivot.Y*rect.Height)
 	return rl.NewRectangle(anchorp.X-pivotp.X+rect.X, anchorp.Y-pivotp.Y+rect.Y, rect.Width, rect.Height)
+}
+
+type horizontalLayout struct {
+	bounds   rl.Rectangle
+	spacing  int
+	position int
+}
+
+func HorizontalLayout(bounds rl.Rectangle, spacing int) horizontalLayout {
+	return horizontalLayout{
+		bounds:   bounds,
+		spacing:  spacing,
+		position: 0,
+	}
+}
+
+func (hl *horizontalLayout) Layout(wh rl.Vector2) rl.Rectangle {
+	r := rl.NewRectangle(hl.bounds.X+float32(hl.position), hl.bounds.Y, wh.X, wh.Y)
+	hl.position += int(wh.X) + hl.spacing
+	return r
+}
+
+func (hl *horizontalLayout) Fill(wh rl.Vector2) rl.Rectangle {
+	r := rl.NewRectangle(hl.bounds.X+float32(hl.position), hl.bounds.Y, hl.bounds.Width-float32(hl.position), wh.Y)
+	hl.position = int(hl.bounds.Width)
+	return r
 }
 
 type verticalLayout struct {
@@ -44,8 +72,14 @@ func VerticalLayout(bounds rl.Rectangle, spacing int) verticalLayout {
 	}
 }
 
-func (vl *verticalLayout) Add(wh rl.Vector2) rl.Rectangle {
+func (vl *verticalLayout) Layout(wh rl.Vector2) rl.Rectangle {
 	r := rl.NewRectangle(vl.bounds.X, vl.bounds.Y+float32(vl.position), wh.X, wh.Y)
 	vl.position += int(wh.Y) + vl.spacing
+	return r
+}
+
+func (vl *verticalLayout) Fill(wh rl.Vector2) rl.Rectangle {
+	r := rl.NewRectangle(vl.bounds.X, vl.bounds.Y+float32(vl.position), wh.X, vl.bounds.Height-float32(vl.position))
+	vl.position = int(vl.bounds.Width)
 	return r
 }
