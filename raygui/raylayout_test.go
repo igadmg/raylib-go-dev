fb3d1be92fd9e4fd234829f41f9478aff28798d9
Igor Karatayev <igor.cwer@gmail.com>
2024-05-01 22:03:36 +0300

    mod tidy fixed tests

--- raygui/raylayout_test.go
+++ raygui/raylayout_test.go
@@ -3,32 +3,33 @@ package raygui_test
 import (
 	"testing"
 
-	"github.com/EliCDavis/vector/test"
+	//. "github.com/EliCDavis/vector/test"
 	rg "github.com/gen2brain/raylib-go/raygui"
 	rl "github.com/gen2brain/raylib-go/raylib"
+	. "github.com/gen2brain/raylib-go/raylib/test"
 )
 
 func TestCanvasLayout(t *testing.T) {
 	cl := rg.CanvasLayout(rl.NewRectangle(-50, -50, 200, 200))
 	{
 		r := cl.Layout(rl.AnchorTopLeft, rl.AnchorTopLeft, rl.NewVector2(20, 20))
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -50, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -50, 20, 20), 0.00001)
 	}
 	{
 		r := cl.Layout(rl.AnchorTopRight, rl.AnchorTopRight, rl.NewVector2(20, 20))
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(130, -50, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(130, -50, 20, 20), 0.00001)
 	}
 	{
 		r := cl.Layout(rl.AnchorTopLeft, rl.AnchorCenter, rl.NewVector2(20, 20))
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(-60, -60, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(-60, -60, 20, 20), 0.00001)
 	}
 	{
 		r := cl.Layout(rl.AnchorBottomLeft, rl.AnchorBottomLeft, rl.NewVector2(20, 20))
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, 130, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, 130, 20, 20), 0.00001)
 	}
 	{
 		r := cl.Layout(rl.AnchorBottomRight, rl.AnchorBottomRight, rl.NewVector2(20, 20))
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(130, 130, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(130, 130, 20, 20), 0.00001)
 	}
 }
 
@@ -36,19 +37,19 @@ func TestVerticalLayout(t *testing.T) {
 	vl := rg.VerticalLayout(rl.NewRectangle(-50, -50, 200, 200), 0)
 	{
 		r := vl.Layout(rl.NewVector2(100, 10), rg.JustifyLeft)
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -50, 100, 10), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -50, 100, 10), 0.00001)
 	}
 	{
 		r := vl.Layout(rl.NewVector2(100, 15), rg.JustifyRight)
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(50, -40, 100, 15), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(50, -40, 100, 15), 0.00001)
 	}
 	{
 		r := vl.Layout(rl.NewVector2(100, 20), rg.JustifyCenter)
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(0, -25, 100, 20), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(0, -25, 100, 20), 0.00001)
 	}
 	{
 		r := vl.Layout(rl.NewVector2(100, 25), rg.JustifyFill)
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -5, 200, 25), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -5, 200, 25), 0.00001)
 	}
 }
 
@@ -56,18 +57,18 @@ func TestVerticalLayoutSpacing5(t *testing.T) {
 	vl := rg.VerticalLayout(rl.NewRectangle(-50, -50, 200, 200), 5)
 	{
 		r := vl.Layout(rl.NewVector2(100, 10), rg.JustifyLeft)
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -50, 100, 10), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -50, 100, 10), 0.00001)
 	}
 	{
 		r := vl.Layout(rl.NewVector2(100, 15), rg.JustifyRight)
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(50, -35, 100, 15), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(50, -35, 100, 15), 0.00001)
 	}
 	{
 		r := vl.Layout(rl.NewVector2(100, 20), rg.JustifyCenter)
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(0, -15, 100, 20), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(0, -15, 100, 20), 0.00001)
 	}
 	{
 		r := vl.Layout(rl.NewVector2(100, 25), rg.JustifyFill)
-		test.AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, 10, 200, 25), 0.00001)
+		AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, 10, 200, 25), 0.00001)
 	}
 }
--- raylib/go.mod
+++ raylib/go.mod
@@ -7,7 +7,13 @@ replace github.com/EliCDavis/vector => ../vector
 require (
 	github.com/EliCDavis/vector v1.6.0
 	github.com/ebitengine/purego v0.7.1
+	github.com/stretchr/testify v1.9.0
 	golang.org/x/sys v0.19.0
 )
 
-require golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20240416160154-fe59bbe5cc7f // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
