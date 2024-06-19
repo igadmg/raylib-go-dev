aee87ed32eeba9c71d24692921c0e34a248682f2
Igor Karatayev <igor.cwer@gmail.com>
2024-06-20 01:50:08 +0300

    Added tests for Spread

--- raygui/raylayout_test.go
+++ raygui/raylayout_test.go
@@ -9,27 +9,45 @@ import (
 	"github.com/igadmg/raylib-go/raymath/vector2"
 )
 
+func TestSpread(t *testing.T) {
+	for p, i := range rg.Spread([]int{0, 1, 2, 3, 4}, vector2.NewFloat32(10, 10), vector2.NewFloat32(90, 10), 20, 10, rg.JustifyLeft) {
+		AssertVector2InDelta(t, vector2.NewFloat32(10+i*20, 10), p, 0.00001)
+	}
+
+	for p, i := range rg.Spread([]int{0, 1, 2, 3, 4, 5, 6, 7, 8}, vector2.NewFloat32(10, 10), vector2.NewFloat32(90, 10), 20, 10, rg.JustifyLeft) {
+		AssertVector2InDelta(t, vector2.NewFloat32(10+i*10, 10), p, 0.00001)
+	}
+
+	for p, i := range rg.Spread([]int{0, 1}, vector2.NewFloat32(10, 10), vector2.NewFloat32(90, 10), 20, 10, rg.JustifyCenter) {
+		AssertVector2InDelta(t, vector2.NewFloat32(40+i*20, 10), p, 0.00001)
+	}
+
+	for p, i := range rg.Spread([]int{0, 1}, vector2.NewFloat32(10, 10), vector2.NewFloat32(90, 10), 20, 10, rg.JustifyRight) {
+		AssertVector2InDelta(t, vector2.NewFloat32(70+i*20, 10), p, 0.00001)
+	}
+}
+
 func TestCanvasLayout(t *testing.T) {
 	cl := rg.CanvasLayout(rl.NewRectangle(-50, -50, 200, 200))
 	{
 		r := cl.Layout(rl.AnchorTopLeft, rl.AnchorTopLeft, vector2.NewFloat32(20, 20))
-		AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, -50, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, rl.NewRectangle(-50, -50, 20, 20), r, 0.00001)
 	}
 	{
 		r := cl.Layout(rl.AnchorTopRight, rl.AnchorTopRight, vector2.NewFloat32(20, 20))
-		AssertRectangle2InDelta(t, r, rl.NewRectangle(130, -50, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, rl.NewRectangle(130, -50, 20, 20), r, 0.00001)
 	}
 	{
 		r := cl.Layout(rl.AnchorTopLeft, rl.AnchorCenter, vector2.NewFloat32(20, 20))
-		AssertRectangle2InDelta(t, r, rl.NewRectangle(-60, -60, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, rl.NewRectangle(-60, -60, 20, 20), r, 0.00001)
 	}
 	{
 		r := cl.Layout(rl.AnchorBottomLeft, rl.AnchorBottomLeft, vector2.NewFloat32(20, 20))
-		AssertRectangle2InDelta(t, r, rl.NewRectangle(-50, 130, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, rl.NewRectangle(-50, 130, 20, 20), r, 0.00001)
 	}
 	{
 		r := cl.Layout(rl.AnchorBottomRight, rl.AnchorBottomRight, vector2.NewFloat32(20, 20))
-		AssertRectangle2InDelta(t, r, rl.NewRectangle(130, 130, 20, 20), 0.00001)
+		AssertRectangle2InDelta(t, rl.NewRectangle(130, 130, 20, 20), r, 0.00001)
 	}
 }
 
--- raylib/go.mod
+++ raylib/go.mod
@@ -11,13 +11,10 @@ replace github.com/igadmg/goex => ../../goex
 replace github.com/DeedleFake/xiter => ../../xiter
 
 require (
+	github.com/DeedleFake/xiter v0.0.0-20240215152241-9fc873aaff80
 	github.com/ebitengine/purego v0.7.1
 	github.com/igadmg/raylib-go/raymath v1.6.0
 	golang.org/x/sys v0.21.0
 )
 
-require (
-	github.com/DeedleFake/xiter v0.0.0-20240215152241-9fc873aaff80 // indirect
-	github.com/igadmg/goex v0.0.0-20240505224334-5f13a70c4df7 // indirect
-	golang.org/x/exp v0.0.0-20240604190554-fc45aab8b7f8 // indirect
-)
+require golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8 // indirect
