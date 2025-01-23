305fe107618ea8b551f8103e776da70103d558dd
Igor Karatayev <igor.cwer@gmail.com>
2025-01-23 20:48:28 +0300

    Rectangle iteration functions.

---
+++ raymath/rect2/rect2.iter.go
@@ -0,0 +1,19 @@
+package rect2
+
+import (
+	"iter"
+
+	"github.com/igadmg/raylib-go/raymath/vector2"
+)
+
+func (r Rectangle[T]) EachUnitCell() iter.Seq[vector2.Vector[T]] {
+	return func(yield func(vector2.Vector[T]) bool) {
+		for x := T(0); x < r.size.X(); x++ {
+			for y := T(0); y < r.size.Y(); y++ {
+				if !yield(vector2.New[T](r.position.X()+x, r.position.X()+y)) {
+					return
+				}
+			}
+		}
+	}
+}
